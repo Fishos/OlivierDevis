47fc1dfc201b29555a8df7679a924d225c9fb178
Olivier Devis <devis.o@hotmail.com>
2019-01-27 18:43:31 +0100

    core ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'resume', to: 'pages#resume', as: :resume
+  root to: 'pages#resume'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
